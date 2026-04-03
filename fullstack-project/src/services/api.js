3ca1685776d0ff3b4fcd380f9aab6ef01f9dcb78
kl2400030358 <2400030358@kluniversity.in>
2026-04-04 03:15:04 +0530

    Revert API base URLs to localhost for local development

--- fullstack-project/src/services/api.js
+++ fullstack-project/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://placement-training-system-1-g5sq.onrender.com/api',
+    baseURL: 'http://localhost:8080/api',
     headers: {
         'Content-Type': 'application/json',
     },
